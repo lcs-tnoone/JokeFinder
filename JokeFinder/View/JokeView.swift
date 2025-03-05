6e437b6ff41e0352147c1a2c9e54e25682ff58c7
Thomas Noone <tnoone@lcs.on.ca>
2025-03-05 10:57:09 -0500

    Added a model to match the structure of the JSON object received from our endpoint.

---
+++ JokeFinder/View/JokeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeFinder
+//
+//  Created by Thomas Noone on 2025-03-05.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    JokeView()
+}
