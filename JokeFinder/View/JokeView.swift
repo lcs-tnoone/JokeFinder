bd87d759171802226f43788a5a1c52383640a34a
Thomas Noone <tnoone@lcs.on.ca>
2025-03-05 19:35:58 -0500

    Improved view so that the punchline doesn't show up right away.

--- JokeFinder/View/JokeView.swift
+++ JokeFinder/View/JokeView.swift
@@ -12,7 +12,14 @@ struct JokeView: View {
     // MARK: Stored properties
     
     // Create the view model (temporarily show the default joke)
-    @State var viewModel = JokeViewModel(currentJoke: exampleJoke)
+    @State var viewModel = JokeViewModel()
+    
+    // Controls punchline visibility
+    @State var punchlineOpacity = 0.0
+
+    // Starts a timer to wait on revealing punchline
+    @State var punchlineTimer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+
     
     // MARK: Computed properties
     var body: some View {
@@ -26,7 +33,18 @@ struct JokeView: View {
                         .padding(.bottom, 100)
                     
                     Text(currentJoke.punchline ?? "")
- 
+                        .opacity(punchlineOpacity)
+                        .onReceive(punchlineTimer) { _ in
+                            
+                            withAnimation {
+                                punchlineOpacity = 1.0
+                            }
+                            
+                            // Stop the timer
+                            punchlineTimer.upstream.connect().cancel()
+                        }
+
+
                 }
                 .font(.title)
                 .multilineTextAlignment(.center)
