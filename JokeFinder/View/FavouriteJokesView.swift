3411cef24ec23aeeb66cc11dfdb9c8ba232a6185
Thomas Noone <tnoone@lcs.on.ca>
2025-03-06 10:06:05 -0500

    Added a tab view to switch between getting new jokes and reviewing favourites.

---
+++ JokeFinder/View/FavouriteJokesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteJokesView.swift
+//  JokeFinder
+//
+//  Created by Thomas Noone on 2025-03-06.
+//
+
+import SwiftUI
+
+struct FavouriteJokesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteJokesView()
+}
---
+++ JokeFinder/View/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  JokeFinder
+//
+//  Created by Thomas Noone on 2025-03-06.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
