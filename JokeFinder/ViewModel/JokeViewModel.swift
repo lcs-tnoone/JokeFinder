0dbafffbdf9b6917c479fef2d584d8174eb79c66
Thomas Noone <tnoone@lcs.on.ca>
2025-03-05 19:12:50 -0500

    Added a basic view model to hold the current joke that will be shown by the view.

---
+++ JokeFinder/ViewModel/JokeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  JokeViewModel.swift
+//  JokeFinder
+//
+//  Created by Thomas Noone on 2025-03-05.
+//
+
+import SwiftUI
+
+struct JokeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JokeViewModel()
+}
